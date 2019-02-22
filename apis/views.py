cffd72240d7b4997a6c8b9d012086ae73f3143f1
saralnigam <saral.nigam@hashedin.com>
2019-02-22 12:14:42 +0530

    Added filtering and search to issues API

--- apis/views.py
+++ apis/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 
 from apis.models import Issue
 from apis.serializers import IssueSerializer
@@ -14,3 +16,6 @@ class UserViewSet(viewsets.ModelViewSet):
 class IssuesViewSet(viewsets.ModelViewSet):
     queryset = Issue.objects.all().select_related('assignee').select_related('created_by')
     serializer_class = IssueSerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter,)
+    filter_fields = ('short_id', 'priority', 'status', 'created_by', 'assignee')
+    search_fields = ('title',)
